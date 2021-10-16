54cef45970f66ca6ad7e60f75e9e984fd757fd4e
RiverRipple <Kanishq0106@gmail.com>
2021-10-15 22:14:56 -0400

    Config and foundations

---
+++ main.js
@@ -0,0 +1,34 @@
+const { Client, Collection, Intents } = require("discord.js");
+const fs = require("fs");
+
+const config = require("./config.json");
+
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS],
+	allowedMentions: { parse: [] },
+});
+
+client.admins = config.admins;
+client.commands = new Collection();
+
+const commandFiles = fs
+	.readdirSync("./commands")
+	.filter((file) => file.endsWith(".js"));
+const eventFiles = fs
+	.readdirSync("./events")
+	.filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+
+client.login(config.token);
